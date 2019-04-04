b4ccc2cc44f29642ad4bd7bcae460ae4704731d3
known <knownchen@163.com>
2019-04-04 22:07:43 +0800

    Updated app

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,11 @@
 //index.js
 //获取应用实例
 const app = getApp()
+const util = require('../../utils/util.js')
 
 Page({
   data: {
+    time: util.formatTime(new Date()),
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
