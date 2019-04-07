f802d7754142e618153925569d408e88694b6c8f
known <knownchen@163.com>
2019-04-07 16:06:06 +0800

    Added swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,8 +5,12 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    time: util.formatTime(new Date()),
-    motto: 'Hello World'
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500
   },
   onLoad: function () {
     
